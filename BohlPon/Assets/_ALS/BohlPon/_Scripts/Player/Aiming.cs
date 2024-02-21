da285a0d7f2525392747b8d4bb3f993024b1dfbb
McKade <10718916@my.uvu.edu>
2024-02-21 00:11:33 -0700

    Added Aiming
    It was more of a headache than I thought lol

--- BohlPon/Assets/_ALS/BohlPon/_Scripts/Player/Aiming.cs
+++ BohlPon/Assets/_ALS/BohlPon/_Scripts/Player/Aiming.cs
@@ -6,6 +6,7 @@ public class Aiming : MonoBehaviour
 {
 	[SerializeField] private GameObject _source;
 	[SerializeField] private GameObject _target;
+	[SerializeField] private LayerMask _raycastLayer;
 
 	[SerializeField] private PlayerData _playerData;
 
@@ -30,6 +31,42 @@ public class Aiming : MonoBehaviour
 		else
 		{
 			_playerIsAiming = false;
+		}
+	}
+
+	private void Update()
+	{
+		if (!_playerIsAiming) return;
+		MoveTarget();
+		AimSource();
+	}
+
+	void MoveTarget()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, Mathf.Infinity, _raycastLayer))
+		{
+			Vector3 hoverPoint = hit.point;
+			_target.transform.position = hoverPoint;
+		}
+	}
+
+	void AimSource()
+	{
+		if(_source != null)
+		{
+			Vector3 direction = _target.transform.position - _source.transform.position;
+
+			if(direction != Vector3.zero)
+			{
+				Quaternion lookRot = Quaternion.LookRotation(direction);
+				float offset = lookRot.eulerAngles.y > 100 ? 90+(90-lookRot.eulerAngles.x) : lookRot.eulerAngles.x;
+				Debug.Log($"Look rot x: {lookRot.eulerAngles.x}");
+				_source.transform.rotation = Quaternion.Euler(offset, /*lookRot.eulerAngles.x*/90f, 0f);
+				//_source.transform.rotation = lookRot;
+			}
 		}
 	}
 }
