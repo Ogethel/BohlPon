9fa378757c61590d2274628665ebcbcf886c542c
McKade <10718916@my.uvu.edu>
2024-02-22 23:34:48 -0700

    Testing Odin

---
+++ BohlPon/Assets/_ALS/BohlPon/_Scripts/OdinTesting.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+[System.Serializable]
+public class CustomData
+{
+    public string ID;
+    public int Quantity;
+}
+
+public class OdinTesting : SerializedMonoBehaviour
+{
+    public Dictionary<int, CustomData> MyDictionary = new(); //new Dictionary<string, int>();
+    public string ButtonName = "Dynamic button name";
+    public bool Toggle;
+
+    [Button("$ButtonName")]
+    private void DefaultSizedButton()
+    {
+        this.Toggle = !this.Toggle;
+    }
+}
