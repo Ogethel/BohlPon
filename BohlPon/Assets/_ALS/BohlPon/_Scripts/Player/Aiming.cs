d2b9b0045b98a92c3fec9e68b1da301d82e4bf97
McKade <10718916@my.uvu.edu>
2024-02-22 23:05:36 -0700

    Adding Projectiles

--- BohlPon/Assets/_ALS/BohlPon/_Scripts/Player/Aiming.cs
+++ BohlPon/Assets/_ALS/BohlPon/_Scripts/Player/Aiming.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Aiming : MonoBehaviour
 {
-	[SerializeField] private GameObject _source;
-	[SerializeField] private GameObject _target;
+	[SerializeField] private GameObject _source; //Ball Launcher
+	[SerializeField] private GameObject _target; //Point we want to look at
 	[SerializeField] private LayerMask _raycastLayer;
+
+	[SerializeField] private ProjectileLauncher _launcher; 
 
 	[SerializeField] private PlayerData _playerData;
 
@@ -36,36 +38,49 @@ public class Aiming : MonoBehaviour
 
 	private void Update()
 	{
+		//! = NOT in C# and we can shorten != true to !nameOfBool
 		if (!_playerIsAiming) return;
+		// (||) == OR operator
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+		{
+			_playerData.PlayerState = PlayerState.PENDING;
+			//Pass in direction described as a Vector3
+			_launcher.LaunchProjectile(_source.transform.forward);
+		}
 		MoveTarget();
 		AimSource();
 	}
 
 	void MoveTarget()
 	{
+		if (_target == null) return;
+		//Also get the position of a tapped finger
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
 
 		if (Physics.Raycast(ray, out hit, Mathf.Infinity, _raycastLayer))
 		{
-			Vector3 hoverPoint = hit.point;
+			Vector3 hoverPoint = hit.point; //3 Float compoents usually described as (x, y, z)
 			_target.transform.position = hoverPoint;
 		}
 	}
 
 	void AimSource()
 	{
-		if(_source != null)
-		{
-			Vector3 direction = _target.transform.position - _source.transform.position;
+		if (_source == null) return;
 
-			if(direction != Vector3.zero)
-			{
-				Quaternion lookRot = Quaternion.LookRotation(direction);
-				//This is an interesting bit of code that we'll want to look at together Pax
-				float offset = lookRot.eulerAngles.y > 100 ? 90+(90-lookRot.eulerAngles.x) : lookRot.eulerAngles.x;
-				_source.transform.rotation = Quaternion.Euler(offset, 90f, 0f);
-			}
+		//direction in vector form that is at the source position looking at the target
+		Vector3 direction = _target.transform.position - _source.transform.position;
+
+		if(direction != Vector3.zero)
+		{
+			//Quaternion = Vector4
+			Quaternion lookRot = Quaternion.LookRotation(direction); 
+			//This is an interesting bit of code that we'll want to look at together Pax
+			//(variable) = (bool) ? (value to set if bool is true) : (value to set if bool is false)
+			float offset = lookRot.eulerAngles.y > 100 ? 90+(90-lookRot.eulerAngles.x) : lookRot.eulerAngles.x;
+			//Set the offset to euler.x and give euler.y and euler.z a fixed value
+			_source.transform.rotation = Quaternion.Euler(offset, 90f, 0f);
 		}
 	}
 }
