10e08db61fa5d4888cb933bf406869045787e480
Ogethel <46454704+Ogethel@users.noreply.github.com>
2024-04-29 21:58:06 -0600

    Adding RNG Needs for testing

---
+++ BohlPon/Packages/com.starphaselab.rngneeds/Editor/ItemPropertyCache.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+namespace RNGNeeds.Editor
+{
+    public class ItemPropertyCache
+    {
+        public SerializedProperty p_Value;
+        public SerializedProperty p_Enabled;
+        public SerializedProperty p_Locked;
+        public SerializedProperty p_InfluenceProvider;
+        public SerializedProperty p_InfluenceSpread;
+        public SerializedProperty p_InvertInfluence;
+    }
+}
