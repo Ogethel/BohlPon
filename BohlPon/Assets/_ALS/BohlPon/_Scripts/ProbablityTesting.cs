d04e63382b8d1cc3362a256c9084359b30c363df
Ogethel <46454704+Ogethel@users.noreply.github.com>
2024-04-29 22:33:38 -0600

    Testing RNG needs

---
+++ BohlPon/Assets/_ALS/BohlPon/_Scripts/ProbablityTesting.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RNGNeeds;
+
+public class ProbablityTesting : MonoBehaviour
+{
+    public ProbabilityList<string> _testingList;
+
+    //List<string> results = _testingList.PickValues;
+}
